38a46e6275c9e9a3ea1522255143c8d277e209a2
CimimUxMaio <cimimuxmaio@gmail.com>
2021-03-04 17:34:20 -0300

    Fix look for env variables as second resource

--- model/args.py
+++ model/args.py
@@ -1,29 +1,27 @@
-import argparse
-import os
-import sys
+import envargparse
 
-parser = argparse.ArgumentParser()
+parser = envargparse.EnvArgParser()
 
-# Positional
+# Mandatory
 name = "inversion"
-parser.add_argument(name, type=float, default=os.environ.get(name.upper()),
+parser.add_argument(f"--{name}", required=True, env_var=name.upper(), type=float,
                         help="The amount of USDT to be used by the bot")
 
 name = "range"
-parser.add_argument(name, type=float, default=os.environ.get(name.upper()),
+parser.add_argument(f"--{name}", required=True, env_var=name.upper(), type=float,
                         help="The range from the starting price (%%) where the bot will operate")
 
 name = "levels"
-parser.add_argument(name, type=int, default=os.environ.get(name.upper()),
+parser.add_argument(f"--{name}", required=True, env_var=name.upper(), type=int,
                         help="The amount of levels to be set on the grid divided by two")
 
 # Optional
 name = "display_graphs"
-parser.add_argument(f"--{name}", action="store_true", default=os.environ.get(name.upper()),
+parser.add_argument(f"--{name}", action="store_true",
                         help="Display the bot's graphs")
 
 name = "starting_price"
-parser.add_argument(f"--{name}", type=float, default=os.environ.get(name.upper()),
+parser.add_argument(f"--{name}", env_var=name.upper(), type=float, nargs="?",
                         help="Starting price at with the bot will start to operate in USDT")
 
 ARGS = parser.parse_args()
